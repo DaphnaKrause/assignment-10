197e02c6bfbada9ea0867f6ab10a1b825e1a928f
Daphna Krause <dmk@cfl.rr.com>
2018-11-06 20:16:44 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// opening
+$(document).ready(() => {
+
+$('#city').on('click', () => {
+   $('.image1').toggle();
+  });
+
+$('#flag').on('click', () => {
+   $('.image2').toggle();
+});
+
+$('#hill').on('click', () => {
+    $('.image3').toggle();
+  });
+
+$('#ruins').on('click', () => {
+    $('.image4').toggle();
+    });
+
+// keep this here
+});
